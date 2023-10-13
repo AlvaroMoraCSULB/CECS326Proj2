21ee075aead4fc70bd63ec37316e7b48e407c845
Alvaro Mora <143311785+AlvaroMoraCSULB@users.noreply.github.com>
2023-10-12 19:05:42 -0700

    Update DiningPhilosophers.java

--- DiningPhilosophers.java
+++ DiningPhilosophers.java
@@ -1,15 +1,13 @@
-/**
- * DiningPhilosophers.java
- *
- * This program starts the dining philosophers problem.
- *
- */
-
-
-public class DiningPhilosophers
-{  
-   public static void main(String args[])
-   {
-     // Here your code
-   }
+public class DiningPhilosophers {
+    public static void main(String[] args) {
+        int numofphilos = 5;
+        DiningServer server = new DiningServerImpl(numofphilos);
+        int x = 0;
+        do{
+            Runnable philosopher = new Philosopher(x, server);
+            Thread philoThread = new Thread(philosopher); // create a thread for each philosopher
+            philoThread.start(); // start the philosopher's thread
+            x++;
+        }while(x < numofphilos);
+    }
 }

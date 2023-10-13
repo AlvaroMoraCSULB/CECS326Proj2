0d6b845a00a98723ac3cdd8c3d2a9fc53ffdf4e4
Alvaro Mora <143311785+AlvaroMoraCSULB@users.noreply.github.com>
2023-10-12 19:07:40 -0700

    Update Philosopher.java

--- Philosopher.java
+++ Philosopher.java
@@ -1,13 +1,30 @@
-/**
- * Philosopher.java
- *
- * This class represents each philosopher thread.
- * Philosophers alternate between eating and thinking.
- *
- */
+public class Philosopher implements Runnable {
+    private int philosopherNumber;
+    private DiningServer server; // DiningServer object for synchronization
+
+    public Philosopher(int philosopherNumber, DiningServer server) { // constructor
+        this.philosopherNumber = philosopherNumber;
+        this.server = server;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                int downtime = (int) (Math.random() * 3000); // random number between 0 and 300ms
+                System.out.println("Philosopher " + philosopherNumber + " took " + downtime + "ms thinking");
+                Thread.sleep(downtime); // put the thread to sleep
+                server.takeForks(philosopherNumber); // request forks
+
+                int sleepTime = (int) (Math.random() * 3000);
+                System.out.println("Philosopher " + philosopherNumber + " took " + sleepTime + "ms eating");
+                Thread.sleep(sleepTime);
+                server.returnForks(philosopherNumber); // release forks
 
 
-public class Philosopher implements Runnable
-{
- //Your code here
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }

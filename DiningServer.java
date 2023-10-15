32961952b03adb6afcfe20c0248df0c833d22018
Jessica G. Banuelos <72938112+jessplease@users.noreply.github.com>
2023-10-15 14:42:19 -0700

    Updated DiningServer.java

--- DiningServer.java
+++ DiningServer.java
@@ -1,12 +1,10 @@
 public interface DiningServer
 {
-    public DiningServerImpl(int numPhilosophers);
     // called by a philosopher when it wishes to eat
     public void takeForks(int philosopherNumber) throws InterruptedException;
 
     // called by a philosopher when it is finished eating
     public void returnForks(int philosopherNumber);
-    private void test(int philosopherNumber);
 
 
 }
